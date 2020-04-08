136c3a469e0b36211c1a6fd3af8f0682fdbd0123
Colin Porter <coln.porter@gmail.com>
2020-04-08 01:05:16 -0700

    handelbars basics added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 7000;
+
+const hbs = require('express-handlebars');
+app.set('view engine', 'hbs');
+
+app.engine( 'hbs', hbs( {
+  extname: "hbs",
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	
+	res.render('main' , {layout: 'index'});
+});
+app.listen( port, () => console.log(`Online at port ${port}`));
--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 7000;
-
-app.get('/', (req, res) => res.send("hello!"));
-
-app.listen( port, () => console.log(`Online at port ${port}`));
